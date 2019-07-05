5f77b187ff5dd6f1e859395450b175b68bcf168f
Harshit Agrawal <harshitag785@gmail.com>
2019-07-05 22:47:44 +0530

    Cart Functionality working fine

--- controllers/admin.js
+++ controllers/admin.js
@@ -16,8 +16,8 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, price, description, imageUrl);
-    product.save()
+  const product = new Product(title, price, description, imageUrl, null, req.user._id);
+  product.save()
     .then(result => {
       console.log(result);
       res.redirect('/admin/products');
@@ -57,10 +57,10 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
- 
-    const product = new Product(updatedTitle, updatedPrice, updatedDesc, updatedImageUrl
-      , new mongodb.ObjectId(prodId));
-    product.save()
+
+  const product = new Product(updatedTitle, updatedPrice, updatedDesc, updatedImageUrl
+    , new mongodb.ObjectId(prodId));
+  product.save()
     .then(result => {
       console.log('PRODUCT UPDATED')
       res.redirect('/admin/products');
