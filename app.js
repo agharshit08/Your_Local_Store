abd28b8ee36ff859fb0cd80fce8e10d271c3af39
Harshit Agrawal <harshitag785@gmail.com>
2019-07-14 11:09:52 +0530

    Serving images statically

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({storage: fileStorage}).single('image'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use(
   session({
