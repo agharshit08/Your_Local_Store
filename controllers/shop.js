81cf70b53124a204979eae52275562c7e596f03c
Harshit Agrawal <harshitag785@gmail.com>
2019-07-14 14:22:45 +0530

    Pagination Added

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,14 +6,32 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
+const ITEMS_PER_PAGE = 1;
+
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
+  .countDocuments()
+  .then(numProducts => {
+    totalItems = numProducts;
+    return Product.find()
+      .skip((page - 1) * ITEMS_PER_PAGE)
+      .limit(ITEMS_PER_PAGE);
+  })
     .then(products => {
-      console.log(products);
       res.render('shop/product-list', {
         prods: products,
-        pageTitle: 'All Products',
-        path: '/products'
+        pageTitle: 'Products',
+        path: '/products',
+        totalProducts: totalItems,
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page+1,
+        previousPage: page-1,
+        lastPage: Math.ceil(totalItems/ ITEMS_PER_PAGE)
       });
     })
     .catch(err => {
@@ -35,17 +53,34 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
+  .countDocuments()
+  .then(numProducts => {
+    totalItems = numProducts;
+    return Product.find()
+      .skip((page - 1) * ITEMS_PER_PAGE)
+      .limit(ITEMS_PER_PAGE);
+  })
     .then(products => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
-        path: '/'
+        path: '/',
+        totalProducts: totalItems,
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page+1,
+        previousPage: page-1,
+        lastPage: Math.ceil(totalItems/ ITEMS_PER_PAGE)
       });
     })
-    .catch(err => {
-      console.log(err);
-    });
+    .catch (err => {
+    console.log(err);
+  });
 };
 
 exports.getCart = (req, res, next) => {
@@ -135,12 +170,12 @@ exports.getInvoice = (req, res, next) => {
       }
       const invoiceName = 'invoice-' + orderId + '.pdf';
       const invoicePath = path.join('data', 'invoices', invoiceName);
-      
+
       const pdfDoc = new PDFDocument();
       res.setHeader('Content-Type', 'application/pdf');
       res.setHeader(
         'Content-Disposition',
-        'inline; filename="' + invoiceName +'" '
+        'inline; filename="' + invoiceName + '" '
       );
       pdfDoc.pipe(fs.createWriteStream(invoicePath));
       pdfDoc.pipe(res);
@@ -156,7 +191,7 @@ exports.getInvoice = (req, res, next) => {
       });
       pdfDoc.fontSize(20).text('Total Price: $' + totalPrice);
       pdfDoc.end();
-      
+
     })
     .catch(err => next(err));
 };
